fedfc595b88f2d4d5d6efb930049857f446e2945
Hachi-R <drive8hachi@gmail.com>
2024-05-29 00:18:14 -0300

    theme mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next';
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const fontSans = FontSans({
-    subsets: ["latin"],
-    variable: "--font-sans",
-})
+    subsets: ['latin'],
+    variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -20,10 +21,19 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={cn(
-                'min-h-screen bg-background font-sans antialiased',
-                fontSans.variable
-            )}>{children}</body>
+            <body
+                className={cn(
+                    'min-h-screen bg-background font-sans antialiased',
+                    fontSans.variable
+                )}>
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='system'
+                    enableSystem
+                    disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
